2d8ce204424a7f59d49ce121e266d33c3bced150
razvan <razzvan.savu@gmail.com>
2024-01-13 13:58:30 +0200

    Display menu open icon

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,5 @@
 function openMenu() {
     document.querySelector('header > nav').style.display = 'flex';
+    document.querySelector('header > section > img.open').style.display = 'none';
+    document.querySelector('header > section > img.close').style.display = 'block';
 }
