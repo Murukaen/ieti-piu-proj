a21041265565eb176070282d2757659209dac914
razvan <razzvan.savu@gmail.com>
2024-01-13 14:36:24 +0200

    Add close menu func.

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,3 +3,9 @@ function openMenu() {
     document.querySelector('header > section > img.open').style.display = 'none';
     document.querySelector('header > section > img.close').style.display = 'block';
 }
+
+function closeMenu() {
+    document.querySelector('header > section > img.close').style.display = 'none';
+    document.querySelector('header > section > img.open').style.display = 'block';
+    document.querySelector('header > nav').style.display = 'none';
+}
