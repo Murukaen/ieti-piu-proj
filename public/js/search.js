1295e8e75fa820308849870d6549fcd1ffab08ae
razvan <razzvan.savu@gmail.com>
2024-01-14 12:49:08 +0200

    Add func. to close the search filters

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,7 @@
 function displaySearchFilter() {
     document.querySelector('#search-section div.filter').style.display = 'block';
 }
+
+function applySearchFilter() {
+    document.querySelector('#search-section div.filter').style.display = 'none';
+}
