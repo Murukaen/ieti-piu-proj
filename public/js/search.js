a43271f53815866fd6e439bbc7180a7de4c6b473
razvan <razzvan.savu@gmail.com>
2024-02-10 21:37:52 +0200

    Create link to my-books page with appropriate filter

--- public/js/search.js
+++ public/js/search.js
@@ -5,3 +5,16 @@ function displaySearchFilter() {
 function applySearchFilter() {
     document.querySelector('#search-section div.filter').style.display = 'none';
 }
+
+addEventListener("load", () => {
+    // attempt extract query from URL
+    let words = window.location.href.split('?');
+    if (words.length > 1) {
+        const query = words[1];
+        if (query == 'f=mybooks') { 
+            // Display filter with my-books options checked
+            displaySearchFilter();
+            document.getElementById('my-books-input').checked = true;
+        }
+    }
+})
